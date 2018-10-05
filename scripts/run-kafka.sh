1ac123367da6b6080dc4d330512538ffa889b483
vadimb <vadim.barylo@intapp.com>
2018-10-05 12:39:40 +0300

    update doc

--- scripts/run-kafka.sh
+++ scripts/run-kafka.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-docker run -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=192.168.99.100 --env ADVERTISED_PORT=9092 spotify/kafka
+#docker run -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=192.168.99.100 --env ADVERTISED_PORT=9092 --env TOPICS=topic1 spotify/kafka
+
+git config --global core.autocrlf false
+git clone https://github.com/wurstmeister/kafka-docker
+
+cd kafka-docker
+docker-compose up
